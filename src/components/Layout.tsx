c08519523c50d8448c6c72c737fa3f6cd43a645b
Andris Borbás <andris.borbas@gmail.com>
2021-02-14 15:36:38 +0100

    Change description

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,13 +15,13 @@ export default function Layout({
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<meta
 					name="description"
-					content="Magyarország legnagyobb egyetemi hallgatók által szervezett éves technológiai konferenciája. Várunk Téged is április 14-én."
+					content="Magyarország legnagyobb egyetemi hallgatók által szervezett éves technológiai konferenciája. Várunk Téged is április 14-én!"
 				/>
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:title" content="18. Simonyi Konferencia" />
 				<meta
 					name="twitter:description"
-					content="Magyarország legnagyobb egyetemi hallgatók által szervezett éves technológiai konferenciája. Várunk Téged is április 14-én."
+					content="Magyarország legnagyobb egyetemi hallgatók által szervezett éves technológiai konferenciája. Várunk Téged is április 14-én!"
 				/>
 				<meta
 					name="twitter:image"
@@ -38,7 +38,7 @@ export default function Layout({
 				<meta property="og:title" content="18. Simonyi Konferencia" />
 				<meta
 					property="og:description"
-					content="Magyarország legnagyobb egyetemi hallgatók által szervezett éves technológiai konferenciája. Várunk Téged is április 14-én."
+					content="Magyarország legnagyobb egyetemi hallgatók által szervezett éves technológiai konferenciája. Várunk Téged is április 14-én!"
 				/>
 				<meta property="og:url" content="https://konferencia.simonyi.bme.hu" />
 				<meta property="og:type" content="website" />
