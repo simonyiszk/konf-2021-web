2161c9e484a64feb7b0561764547edf1bc3cb056
Andris Borbás <andris.borbas@gmail.com>
2021-02-14 16:50:22 +0100

    Add vercel sponsor

--- src/utils/contentful.ts
+++ src/utils/contentful.ts
@@ -1,12 +1,11 @@
 import { createClient } from "contentful";
 
 export const client = createClient({
-	space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID ?? "ErrorNoSpaceID",
+	space: process.env.CONTENTFUL_SPACE_ID ?? "ErrorNoSpaceID",
 	accessToken:
 		(process.env.VERCEL_ENV === "production"
-			? process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
-			: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN) ??
-		"ErrorNoAccessToken",
+			? process.env.CONTENTFUL_ACCESS_TOKEN
+			: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN) ?? "ErrorNoAccessToken",
 	host:
 		process.env.VERCEL_ENV === "production"
 			? "cdn.contentful.com"
