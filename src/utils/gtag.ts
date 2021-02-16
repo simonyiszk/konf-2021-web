052791d2c7069030e61003d6705e21c7d3a4a852
Andris Borbás <andris.borbas@gmail.com>
2021-02-16 01:17:57 +0100

    Change google tag to universal

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-SNBY06N2ET";
+export const GA_TRACKING_ID = "UA-189680912-2";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
