ca40de8e16b490a1b5b25fedaa48236880637f1e
Andris Borbás <andris.borbas@gmail.com>
2021-03-16 04:34:31 +0100

    Flip ordering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ export const getStaticProps = async () => {
 		a: Entry<IPresentationFields>,
 		b: Entry<IPresentationFields>,
 	): number {
-		if (a.fields.order > b.fields.order) return -1;
-		if (a.fields.order < b.fields.order) return 1;
+		if (a.fields.order > b.fields.order) return 1;
+		if (a.fields.order < b.fields.order) return -1;
 		return 0;
 	}
 
