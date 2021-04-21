3d19f501837ef5eb4887f95a559d2f43e224229b
Andris Borbás <andris.borbas@gmail.com>
2021-04-21 03:07:51 +0200

    Add youtube videos to cards

--- src/components/presentations/PresentationCard.tsx
+++ src/components/presentations/PresentationCard.tsx
@@ -2,9 +2,11 @@ import clsx from "clsx";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { forwardRef } from "react";
+import YouTube from "react-youtube";
 import tinydate from "tinydate";
 
 import type { IPresentationFields } from "@/@types/generated/contentful";
+import { parseYoutubeIdFromLink } from "@/utils/utils";
 
 import styles from "./PresentationCard.module.scss";
 
@@ -23,6 +25,7 @@ const PresentationCard = forwardRef<HTMLDivElement, PresentationCardProps>(
 			startDate,
 			endDate,
 			profession,
+			link,
 			imageURL,
 			isLeft,
 			children,
@@ -34,6 +37,8 @@ const PresentationCard = forwardRef<HTMLDivElement, PresentationCardProps>(
 		const startDateObj = new Date(startDate ?? "");
 		const endDateObj = new Date(endDate ?? "");
 		const stamp = tinydate("{HH}:{mm}");
+
+		const ytId = parseYoutubeIdFromLink(link ?? "");
 
 		return (
 			<motion.figure className={clsx(styles.card, className)} ref={ref}>
@@ -78,7 +83,14 @@ const PresentationCard = forwardRef<HTMLDivElement, PresentationCardProps>(
 							</h5>
 						)}
 					</figcaption>
-
+					{ytId && (
+						<div className="col-span-2 mt-2 w-full">
+							<YouTube
+								videoId={ytId}
+								containerClassName="aspect-w-16 aspect-h-9"
+							/>
+						</div>
+					)}
 					<div className={styles.content}>{children}</div>
 				</div>
 			</motion.figure>
